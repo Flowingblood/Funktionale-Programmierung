c318faa535704f54d35012cdf9c352d0bfeb7647
Flowingblood <da.ka@freenet.de>
2022-01-15 02:21:44 +0100

    Create Test26.rkt

---
+++ Test26.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require rackunit)
+
+(define-syntax (infix stx)
+  (define args (cdr (syntax->datum stx)))
+
+  (define sum 0)
+  
+  (for ([i (- (round (/ (length args)2)) 1)])
+                                  (set! sum ((eval (list-ref args 1)) (list-ref args 0) (list-ref args 2)))
+                                  (set! args (cdddr args))
+                                  (set! args (append (list sum) args))
+    
+   )
+  
+  (datum->syntax stx sum)
+)
+
+
+(check-equal? (infix 1 + 2 + 3 + 4) 10)
+
+(check-equal? (infix 1 - 4 - 6 - 1) -10)
+
+(check-equal? (infix 1 - 2 + 3 - 4) -2)
